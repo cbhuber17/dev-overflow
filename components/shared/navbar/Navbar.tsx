9aace956e85e8fe67ee1a830e71f34c940a6daa2
Colin Huber <cbhuber@gmail.com>
2023-11-23 21:34:24 -0700

    feat: mobile navigation

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
-// import MobileNav from './MobileNav'
+import MobileNav from "./MobileNav";
 // import GlobalSearch from '../search/GlobalSearch'
 
 const Navbar = () => {
@@ -41,7 +41,7 @@ const Navbar = () => {
           />
         </SignedIn>
 
-        {/* <MobileNav /> */}
+        <MobileNav />
       </div>
     </nav>
   );
