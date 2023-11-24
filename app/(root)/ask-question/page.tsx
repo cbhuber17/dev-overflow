ca5e3ac329c3d529110c0aab4fee2a9bbc1ef6d5
Colin Huber <cbhuber@gmail.com>
2023-11-23 18:16:22 -0700

    env fix: update prettier formatting

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>Ask Question</div>
-  )
-}
+  return <div>Ask Question</div>;
+};
 
-export default page
+export default page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter, Space_Grotesk } from 'next/font/google'
-import type { Metadata } from 'next/types'
-import './globals.css'
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next/types";
+import "./globals.css";
 
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter'
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk'
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "DevOverflow",
-  description: "A community-driven platform for asking and answering programming quedstions. Get help, share knowledge, and collaborate with developers from around the world. Explore tools in web development, mobile app developmnent, algorithms, data structures and more.",
+  description:
+    "A community-driven platform for asking and answering programming quedstions. Get help, share knowledge, and collaborate with developers from around the world. Explore tools in web development, mobile app developmnent, algorithms, data structures and more.",
   icons: {
-    icon: "/assets/images/site-logo.svg"
-  }
-}
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
@@ -35,8 +36,7 @@ export default function RootLayout({
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
           {children}
         </body>
- 
       </html>
     </ClerkProvider>
-  )
+  );
 }
