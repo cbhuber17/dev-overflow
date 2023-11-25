6023f674124de670d015459a5d2df8e941621471
Colin Huber <cbhuber@gmail.com>
2023-11-24 23:32:40 -0700

    feat: Home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,151 @@
-export default function Home() {
-  return <div>Home</div>;
+import QuestionCard from "@/components/cards/QuestionCard";
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+// import Pagination from "@/components/shared/Pagination";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
+// import {
+//   getQuestions,
+//   getRecommendedQuestions,
+// } from "@/lib/actions/question.action";
+import { SearchParamsProps } from "@/types";
+import Link from "next/link";
+
+import type { Metadata } from "next";
+// import { auth } from "@clerk/nextjs";
+
+export const metadata: Metadata = {
+  title: "Home | Dev Overflow",
+};
+
+export default async function Home({ searchParams }: SearchParamsProps) {
+  // const { userId } = auth();
+
+  // let result;
+
+  // if (searchParams?.filter === "recommended") {
+  //   if (userId) {
+  //     result = await getRecommendedQuestions({
+  //       userId,
+  //       searchQuery: searchParams.q,
+  //       page: searchParams.page ? +searchParams.page : 1,
+  //     });
+  //   } else {
+  //     result = {
+  //       questions: [],
+  //       isNext: false,
+  //     };
+  //   }
+  // } else {
+  //   result = await getQuestions({
+  //     searchQuery: searchParams.q,
+  //     filter: searchParams.filter,
+  //     page: searchParams.page ? +searchParams.page : 1,
+  //   });
+  // }
+
+  // Temporary
+  const result = {
+    questions: [
+      {
+        _id: "1",
+        title: "Cascading deletes in SQL Alchemy.",
+        tags: [
+          { _id: "1", name: "python" },
+          { _id: "2", name: "sql" },
+        ],
+        author: {
+          _id: "1",
+          name: "Bill Caesar",
+          picture: "",
+          clerkId: "abcd",
+        },
+        upvotes: ["10"],
+        views: 100,
+        answers: [{ num: 2 }],
+        createdAt: new Date(2023, 8, 2),
+      },
+      {
+        _id: "2",
+        title: "How to center a div",
+        tags: [{ _id: "3", name: "css" }],
+        author: {
+          _id: "2",
+          name: "James Frank",
+          picture: "",
+          clerkId: "efgh",
+        },
+        upvotes: ["12"],
+        views: 25,
+        answers: [{ num: 1 }],
+        createdAt: new Date(2023, 10, 12),
+      },
+    ],
+    isNext: false,
+  };
+
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
+          otherClasses="flex-1"
+        />
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+
+      <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There are no question(s) to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. Our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
+      <div className="mt-10">
+        {/* <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        /> */}
+      </div>
+    </>
+  );
 }
