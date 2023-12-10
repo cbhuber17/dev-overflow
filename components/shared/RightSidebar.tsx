96017786b2c53bd038fa8d59cb1ebe47ec93d778
Colin Huber <cbhuber@gmail.com>
2023-12-10 11:48:59 -0700

    feat: Right side bar popular tags.

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,19 +3,11 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
 import { getHotQuestions } from "@/lib/actions/question.action";
-// import { getTopPopularTags } from "@/lib/actions/tag.actions";
-
-const popularTags = [
-  { _id: "1", name: "Typescript", numberOfQuestions: 8 },
-  { _id: "2", name: "C++", numberOfQuestions: 5 },
-  { _id: "3", name: "Javascript", numberOfQuestions: 7 },
-  { _id: "4", name: "vue", numberOfQuestions: 4 },
-  { _id: "5", name: "redux", numberOfQuestions: 10 },
-];
+import { getTopPopularTags } from "@/lib/actions/tag.action";
 
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
-  // const popularTags = await getTopPopularTags();
+  const popularTags = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
