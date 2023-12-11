66492a72ab91df456c92e7f04e4b4bf656d25e3d
Colin Huber <cbhuber@gmail.com>
2023-12-10 19:27:41 -0700

    feat: Home filters implemetation.

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,36 +1,35 @@
 "use client";
 
 import { HomePageFilters } from "@/constants/filters";
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
+import { formUrlQuery } from "@/lib/utils";
 
 const HomeFilters = () => {
-  //   const searchParams = useSearchParams();
-  //   const router = useRouter();
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-  //   const [active, setActive] = useState("");
-  const active = "";
+  const [active, setActive] = useState("");
 
   const handleTypeClick = (item: string) => {
-    // if (active === item) {
-    //   setActive("");
-    //   const newUrl = formUrlQuery({
-    //     params: searchParams.toString(),
-    //     key: "filter",
-    //     value: null,
-    //   });
-    //   router.push(newUrl, { scroll: false });
-    // } else {
-    //   setActive(item);
-    //   const newUrl = formUrlQuery({
-    //     params: searchParams.toString(),
-    //     key: "filter",
-    //     value: item.toLowerCase(),
-    //   });
-    //   router.push(newUrl, { scroll: false });
-    // }
+    if (active === item) {
+      setActive("");
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: null,
+      });
+      router.push(newUrl, { scroll: false });
+    } else {
+      setActive(item);
+      const newUrl = formUrlQuery({
+        params: searchParams.toString(),
+        key: "filter",
+        value: item.toLowerCase(),
+      });
+      router.push(newUrl, { scroll: false });
+    }
   };
 
   return (
