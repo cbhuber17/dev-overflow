e05930bc61332dda4401ceb5fe7d1480bcee7c48
Colin Huber <cbhuber@gmail.com>
2023-12-14 18:38:36 -0700

    fix: Pagination import.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
 import NoResult from "@/components/shared/NoResult";
-// import Pagination from "@/components/shared/Pagination";
+import Pagination from "@/components/shared/Pagination";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
@@ -15,7 +15,6 @@ import Link from "next/link";
 
 import type { Metadata } from "next";
 import { auth } from "@clerk/nextjs";
-import Pagination from "@/components/shared/Pagination";
 
 export const metadata: Metadata = {
   title: "Home | Dev Overflow",
